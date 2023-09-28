1392e93aa6e73bd20ffe93465e551cb50b540dfb
Ng Zhun Onn <ngzhunonn+github@gmail.com>
2023-09-28 17:09:56 +0800

    🎉Assignment done with the completion of thanks page

--- js/script.js
+++ js/script.js
@@ -2,18 +2,6 @@ let basket = JSON.parse(localStorage.getItem("cart")) || [];
 let book = JSON.parse(localStorage.getItem("discount")) || [];
 let person = JSON.parse(localStorage.getItem("user")) || [];
 
-function calculate() {
-    const cartAmount = document.querySelector(".cart-amount");
-    const totalQuantity = basket.reduce((sum, item) => sum + item.num, 0);
-    cartAmount.innerHTML = totalQuantity;
-}
-
-calculate();
-
-function formatNum(num) {
-    return (num).toLocaleString('en', {maximumFractionDigits: 2, minimumFractionDigits: 2})
-}
-
 const userIcon = document.querySelector(".fa-circle-user");
 const user = person.find(user => user.loggedin === true);
 const menu = document.querySelector('.user div');
@@ -39,4 +27,33 @@ function logout() {
         localStorage.setItem("user", JSON.stringify(person));
         location.reload();
     }
+}
+
+function calculate() {
+    const cartAmount = document.querySelector(".cart-amount");
+    const totalQuantity = basket.reduce((sum, item) => sum + item.num, 0);
+    cartAmount.innerHTML = totalQuantity;
+}
+
+calculate();
+
+function formatNum(num) {
+    return (num).toLocaleString('en', {maximumFractionDigits: 2, minimumFractionDigits: 2})
+}
+
+function orderDetails() {
+    const id = document.querySelector(".order-details .id");
+    const date = document.querySelector(".order-details .date");
+    const options = { year:'numeric' ,month:'short', day:'2-digit', hour:"2-digit" ,hour12:true, minute:'2-digit'}
+
+    id.innerHTML = orderID();
+    date.innerHTML  = new Date().toLocaleDateString(undefined, options)
+}
+
+function orderID() {
+    let string = "#"
+    for (let index = 0; index < 8; index++) {
+        string += ~~(Math.random() * 10)
+    }
+    return string
 }
